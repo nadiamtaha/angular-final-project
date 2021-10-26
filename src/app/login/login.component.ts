756b5a2ab291af3772f9c68dc0936593e3ec7bba
ntaha <you@example.com>
2021-10-26 19:51:33 +0200

    push code

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,6 +9,7 @@ import { ToastrService } from 'ngx-toastr';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent implements OnInit {
   loginForm:FormGroup=new FormGroup(
     {
